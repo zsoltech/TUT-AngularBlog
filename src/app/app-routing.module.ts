b60df3122e6c91899e93f53ddf943e1974154a0f
zsoltech <zsoltech@pm.me>
2020-11-06 09:11:18 +0100

    Fix routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,9 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PostsComponent } from './posts/posts.component';
 import { MainComponent } from './main/main.component';
-import { UserComponent } from './user/user.component';
 
 const routes: Routes = [  
   { path: 'posts', component: PostsComponent },  
-  { path: 'users', component: UserComponent },
   { path: '**', component: MainComponent },
 ];
 
